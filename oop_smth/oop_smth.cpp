f61a31448ff0afe7eba545ad3d06ce323b25d00e
r2d2 <r2d2@9E20-777A>
2024-09-24 12:45:21 +0300

    Добавьте файлы проекта.

---
+++ oop_smth/oop_smth.cpp
@@ -0,0 +1,46 @@
+﻿// oop_smth.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <optional>
+#include <string>
+
+using namespace std;
+class LazyInitialisation
+{
+private:
+    string* resource = nullptr;
+    string text;
+    string res;
+    void load(string res)
+    {
+        resource = new string();
+        std::ifstream in(res); 
+        if (in.is_open())
+        {
+            while (getline(in, res))
+            {
+                *resource += res;
+            }
+        };
+        in.close();     
+    }
+public:
+    LazyInitialisation(string t) {
+        text = t;
+    }
+    string get()
+    {
+        if (resource != nullptr)
+            return *resource;
+        load(text);
+        return *resource;
+    }
+};
+int main()
+{
+    LazyInitialisation L("text.txt");
+    cout << L.get()<<'\n';
+}
+
